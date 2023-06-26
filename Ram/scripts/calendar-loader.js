812fb61df54b5df0e3fd641e4fba289e43dfa70d
Ayden1456 <89323066+Ayden1456@users.noreply.github.com>
2023-06-26 14:56:49 -0700

    finish card-view

--- Ram/scripts/calendar-loader.js
+++ Ram/scripts/calendar-loader.js
@@ -1,12 +1,10 @@
-import { Calendar } from '@fullcalendar/core'
-import dayGridPlugin from '@fullcalendar/daygrid'
 
-const calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridWeek',
-    headerToolbar: {
-        left: 'prev,next',
-        center: 'title',
-        right: 'dayGridWeek,dayGridDay' // user can switch between the two
-    }
-})
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
+
